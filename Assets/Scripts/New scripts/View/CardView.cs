ba07dcfaa6bb4b88c7d2d72156ce9250f9c970b9
mu5hjj <james.mushahwar@gmail.com>
2021-09-02 13:10:21 +0100

    card views

---
+++ Assets/Scripts/New scripts/View/CardView.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CardView : MonoBehaviour
+{
+    #region Variables
+    [SerializeField] TextMeshProUGUI cardName;
+    [SerializeField] Sprite runeType; 
+    [SerializeField] Sprite raceType; 
+    [SerializeField] TextMeshProUGUI description; 
+    #endregion
+}
---
+++ Assets/Scripts/New scripts/View/UnitView.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UnitView : CardView
+{
+    [SerializeField] private TextMeshProUGUI slots;
+    [SerializeField] private TextMeshProUGUI cost;
+    [SerializeField] private TextMeshProUGUI attack;
+    [SerializeField] private TextMeshProUGUI health;
+    [SerializeField] private TextMeshProUGUI movementRange;
+    [SerializeField] private TextMeshProUGUI attackRange;
+}
